ea3c053649c34e88cf048c74debd22dd38ee344d
hou <1095080825@qq.com>
2018-01-03 16:51:17 +0800

    路由切换高亮

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 //入口文件
 import Vue from 'vue'
 
+//引入路由的包
+import VueRouter from 'vue-router'
+//挂载到 Vue 上
+Vue.use(VueRouter)
+//导入路由文件
+import router from './router.js'
+
 //导入入口文件
 import App from './App.vue'
+
 
 //导入 mui 文件
 import './lib/mui/css/mui.min.css'
@@ -14,7 +22,8 @@ Vue.component(Header.name, Header)
 
 let vm = new Vue({
     el:'#app',
-    render : c => c(App)
+    render : c => c(App),
+    router
 })
 
 
--- src/router.js
+++ src/router.js
@@ -5,9 +5,10 @@ import VueRouter from 'vue-router'
 
 //创建 router 实例 
 let router = new VueRouter({
-    routes: [
+    routes: [ //配置路由规则
         
-    ]
+    ],
+    linkActiveClass : 'mui-active' //使用mui默认高亮类名 覆盖router-link默认的类名router-link-active
 })
 
 //导出 router 实例
