552db6dedb83d5314b0562bfcd6a376518e94e2d
hou <1095080825@qq.com>
2018-01-05 15:29:55 +0800

    新闻页面模板样式

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -1,6 +1,43 @@
 <template>
     <div>
-        <h1>新闻列表页面</h1>
+        <ul class="mui-table-view">
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="http://dcloud.io/hellomui/images/shuijiao.jpg">
+                    <div class="mui-media-body">
+                        <h1>幸福</h1>
+                        <p class='mui-ellipsis'>
+                            <span>想要这样一间小木屋</span>
+                            <span>被点击了：0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="http://dcloud.io/hellomui/images/shuijiao.jpg">
+                    <div class="mui-media-body">
+                        <h1>木屋</h1>
+                        <p class='mui-ellipsis'>
+                            <span>想要这样一间小木屋</span>
+                            <span>被点击了：0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+            <li class="mui-table-view-cell mui-media">
+                <a href="javascript:;">
+                    <img class="mui-media-object mui-pull-left" src="http://dcloud.io/hellomui/images/shuijiao.jpg">
+                    <div clss="mui-media-body">
+                        <h1>CBD</h1>
+                        <p class='mui-ellipsis'>
+                            <span>想要这样一间小木屋</span>
+                            <span>被点击了：0次</span>
+                        </p>
+                    </div>
+                </a>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -11,5 +48,14 @@
 </script>
 
 <style scoped lang="scss">
-
+    .mui-table-view {
+        h1 {
+            font-size: 14px;
+        }
+        .mui-ellipsis {
+            font-size: 12px;
+            display: flex;
+            justify-content: space-between
+        }
+    }
 </style>
