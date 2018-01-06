174b5cc094d8761d8f76710b0d2a1cef14a96e63
hou <1095080825@qq.com>
2018-01-06 19:44:24 +0800

    商品购买样式完成

--- src/components/goods/GoodsList.vue
+++ src/components/goods/GoodsList.vue
@@ -1,6 +1,49 @@
 <template>
-    <div>
-        <h1>xxx</h1>
+    <div class="goods-list">
+        <div class="goods-item">
+            <img src="http://ofv795nmp.bkt.clouddn.com//upload/201504/20/thumb_201504200119256512.jpg" alt="">
+            <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="goods-item">
+            <img src="http://ofv795nmp.bkt.clouddn.com//upload/201504/20/thumb_201504200214471783.jpg" alt="">
+            <h1 class="title">尼康(Nikon)D3300套机（18-55mm f/3.5-5.6G VRII）（黑色）</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+
+        <div class="goods-item">
+            <img src="http://ofv795nmp.bkt.clouddn.com//upload/201504/20/thumb_201504200119256512.jpg" alt="">
+            <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">￥899</span>
+                    <span class="old">￥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +54,47 @@
 </script>
 
 <style scoped lang="scss">
-
+    .goods-list {
+        padding: 7px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        .goods-item {
+            width: 49%;
+            border: 1px solid #ccc;
+            box-shadow: 0 0 8px #ccc;
+            margin: 4px 0;
+            padding: 2px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            min-height: 293px;
+            img {
+                width: 100%;
+            }
+            .title {
+                font-size: 14px;
+            }
+            .info {
+                background-color: #eee;
+                .price {
+                    .now {
+                        color : red;
+                        font-size: 16px;
+                        font-weight: 700;
+                    }
+                    .old {
+                        font-size: 12px;
+                        text-decoration: line-through;
+                        margin-left: 10px;
+                    }
+                }
+                .sell {
+                    display: flex;
+                    justify-content: space-between;
+                    font-size: 13px;
+                }
+            }
+        }
+    }
 </style>
